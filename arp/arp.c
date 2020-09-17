9ed9c1fc0e1d999f056559589257e89d542b1f79
rochavinicius <vinir.inter@hotmail.com>
2020-09-16 22:26:37 -0300

    Finishing MiTM(Man in The Middle) process.

--- arp/arp.c
+++ arp/arp.c
@@ -9,8 +9,11 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <netinet/ether.h>
+#include <unistd.h>
 #include "arp.h"
 
+/* Constant parameters */
+/* TODO Change destination addresses to be a parameter */
 uint8_t this_mac[6];
 uint8_t bcast_mac[6] =	{0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 uint8_t dst_mac[6] =	{0x00, 0x00, 0x00, 0x22, 0x22, 0x22};
@@ -20,19 +23,17 @@ uint8_t dst_ip[4] = {0x0a, 0x00, 0x00, 0x14}; // 10.0.0.20
 
 union eth_buffer buffer_u;
 
-int main(int argc, char *argv[])
+/* Processes functions, these are independent processes */
+
+int CreateSocket()
 {
 	struct ifreq if_idx, if_mac, ifopts;
 	char ifName[IFNAMSIZ];
-	struct sockaddr_ll socket_address;
-	int sockfd, numbytes, i;
-	struct in_addr addr;
+	//struct sockaddr_ll socket_address;
+	int sockfd;	
 	
 	/* Get interface name */
-	if (argc > 1)
-		strcpy(ifName, argv[1]);
-	else
-		strcpy(ifName, DEFAULT_IF);
+	strcpy(ifName, DEFAULT_IF);
 		
 	/* Open RAW socket */
 	if ((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
@@ -44,14 +45,15 @@ int main(int argc, char *argv[])
 	ifopts.ifr_flags |= IFF_PROMISC;
 	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
 
+	//TODO se pa eh soh pra fazer send
 	/* Get the index of the interface */
 	memset(&if_idx, 0, sizeof(struct ifreq));
 	strncpy(if_idx.ifr_name, ifName, IFNAMSIZ-1);
 	if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0)
 		perror("SIOCGIFINDEX");
-	socket_address.sll_ifindex = if_idx.ifr_ifindex;
-	socket_address.sll_halen = ETH_ALEN;
-
+	//socket_address.sll_ifindex = if_idx.ifr_ifindex;
+	//socket_address.sll_halen = ETH_ALEN;
+	
 	/* Get the MAC address of the interface */
 	memset(&if_mac, 0, sizeof(struct ifreq));
 	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ-1);
@@ -59,43 +61,37 @@ int main(int argc, char *argv[])
 		perror("SIOCGIFHWADDR");	
 	memcpy(this_mac, if_mac.ifr_hwaddr.sa_data, 6);
 	
+	return sockfd;
+}
+
+/**/
+void P1()
+{
+}
+
+/**/
+void P2()
+{
+}
+
+/**/
+void P3()
+{
+	int numbytes, i;
+	struct in_addr addr;
+	int sockfd = CreateSocket();
+	
 	printf("this mac address: ");
 	for (i = 0; i < 5; i++)
 		printf("%02x:", this_mac[i]);
 	printf("%02x", this_mac[5]);
 	printf("\n");
-	    
-	/* End of configuration. Now we can send and receive data using raw sockets. */
-
-
-	/* To send data (in this case we will cook an ARP packet and broadcast it =])... */
-	
-	/* fill the Ethernet frame header */
-	memcpy(buffer_u.cooked_data.ethernet.dst_addr, bcast_mac, 6);
-	memcpy(buffer_u.cooked_data.ethernet.src_addr, src_mac, 6);
-	buffer_u.cooked_data.ethernet.eth_type = htons(ETH_P_ARP);
-
-	/* fill payload data (incomplete ARP request example) */
-	buffer_u.cooked_data.payload.arp.hw_type = htons(1);
-	buffer_u.cooked_data.payload.arp.prot_type = htons(ETH_P_IP);
-	buffer_u.cooked_data.payload.arp.hlen = 6;
-	buffer_u.cooked_data.payload.arp.plen = 4;
-	buffer_u.cooked_data.payload.arp.operation = htons(1);
-	memcpy(buffer_u.cooked_data.payload.arp.src_hwaddr, src_mac, 6);
-	memcpy(buffer_u.cooked_data.payload.arp.src_paddr, src_ip, 6);
-	memset(buffer_u.cooked_data.payload.arp.tgt_hwaddr, 0, 6);
-	memcpy(buffer_u.cooked_data.payload.arp.tgt_paddr, dst_ip, 6);
-
-	/* Send it.. */
-	memcpy(socket_address.sll_addr, dst_mac, 6);
-	if (sendto(sockfd, buffer_u.raw_data, sizeof(struct eth_hdr) + sizeof(struct arp_packet), 0, (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll)) < 0)
-		printf("Send failed\n");
-
 	
 	/* To receive data (in this case we will inspect ARP and IP packets)... */
 
 	while (1){
 		numbytes = recvfrom(sockfd, buffer_u.raw_data, ETH_LEN, 0, NULL, NULL);
+		
 		if (buffer_u.cooked_data.ethernet.eth_type == ntohs(ETH_P_ARP)){
 			printf("******* ARP packet\n");
 			printf("	HW Type: %d\n", buffer_u.cooked_data.payload.arp.hw_type);
@@ -122,7 +118,7 @@ int main(int argc, char *argv[])
 			
 			printf("	Target IP: ");
 			addr.s_addr = *(uint32_t*)&buffer_u.cooked_data.payload.arp.tgt_paddr;
-			printf( "%s\n", inet_ntoa( addr ) );
+			printf( "%s\n", inet_ntoa(addr));
 			
 			continue;
 		}
@@ -138,8 +134,113 @@ int main(int argc, char *argv[])
 			continue;
 		}
 				
-		printf("got a packet, %d bytes\n", numbytes);
+		printf("Got a packet that is not either ARP or IP, %d bytes\n", numbytes);
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct ifreq if_idx, if_mac, ifopts;
+	char ifName[IFNAMSIZ];
+	struct sockaddr_ll socket_address;
+	int sockfd, i;
+	//int numbytes;
+	//struct in_addr addr;
+	
+	/* Get interface name */
+	/*if (argc > 1)
+		strcpy(ifName, argv[1]);
+	else
+		strcpy(ifName, DEFAULT_IF);*/
+		
+	/* Open RAW socket */
+	/*if ((sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
+		perror("socket");*/
+	
+	/* Set interface to promiscuous mode */
+	/*strncpy(ifopts.ifr_name, ifName, IFNAMSIZ-1);
+	ioctl(sockfd, SIOCGIFFLAGS, &ifopts);
+	ifopts.ifr_flags |= IFF_PROMISC;
+	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);*/
+
+	/* Get the index of the interface */
+	/*memset(&if_idx, 0, sizeof(struct ifreq));
+	strncpy(if_idx.ifr_name, ifName, IFNAMSIZ-1);
+	if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0)
+		perror("SIOCGIFINDEX");
+	socket_address.sll_ifindex = if_idx.ifr_ifindex;
+	socket_address.sll_halen = ETH_ALEN;*/
+
+	/* Get the MAC address of the interface */
+	/*memset(&if_mac, 0, sizeof(struct ifreq));
+	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ-1);
+	if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac) < 0)
+		perror("SIOCGIFHWADDR");	
+	memcpy(this_mac, if_mac.ifr_hwaddr.sa_data, 6);
+	
+	printf("this mac address: ");
+	for (i = 0; i < 5; i++)
+		printf("%02x:", this_mac[i]);
+	printf("%02x", this_mac[5]);
+	printf("\n");*/
+	    
+	/* End of configuration. Now we can send and receive data using raw sockets. */
+
+
+	/* To send data (in this case we will cook an ARP packet and broadcast it =])... */
+	
+	/* fill the Ethernet frame header */
+	/*memcpy(buffer_u.cooked_data.ethernet.dst_addr, bcast_mac, 6);
+	memcpy(buffer_u.cooked_data.ethernet.src_addr, src_mac, 6);
+	buffer_u.cooked_data.ethernet.eth_type = htons(ETH_P_ARP);*/
+
+	/* fill payload data (incomplete ARP request example) */
+	/*buffer_u.cooked_data.payload.arp.hw_type = htons(1);
+	buffer_u.cooked_data.payload.arp.prot_type = htons(ETH_P_IP);
+	buffer_u.cooked_data.payload.arp.hlen = 6;
+	buffer_u.cooked_data.payload.arp.plen = 4;
+	buffer_u.cooked_data.payload.arp.operation = htons(1);
+	memcpy(buffer_u.cooked_data.payload.arp.src_hwaddr, src_mac, 6);
+	memcpy(buffer_u.cooked_data.payload.arp.src_paddr, src_ip, 6);
+	memset(buffer_u.cooked_data.payload.arp.tgt_hwaddr, 0, 6);
+	memcpy(buffer_u.cooked_data.payload.arp.tgt_paddr, dst_ip, 6);*/
+
+	/* Send it.. */
+	/*memcpy(socket_address.sll_addr, dst_mac, 6);
+	if (sendto(sockfd, buffer_u.raw_data, sizeof(struct eth_hdr) + sizeof(struct arp_packet), 0, (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll)) < 0)
+		printf("Send failed\n");*/
+
+	P3();
+	/*int pid = fork();
+	
+	if (pid == -1)
+	{
+		printf("Fork failed for first child.\n");
+		exit(1);
+	}
+	
+	if (pid) // P1
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			printf("Fork failed for second child.\n");
+			exit(1);
+		}
+	
+		if (pid) // P1
+		{
+			P1();
+		}
+		else // P3
+		{
+			P3();
+		}
+	}
+	else // P2
+	{
+		P2();
+	}*/	
 
 	return 0;
 }
